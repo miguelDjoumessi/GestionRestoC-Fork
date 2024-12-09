4e5b98303181d377af0c7386c17b6168781f2568
AMDev <miguelawoko1@gmail.com>
2024-12-09 09:34:42 +0100

    Get Upstream since original project

--- obj/Debug/net8.0-windows/PROJET_C#_GESTIONRESTO.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/PROJET_C#_GESTIONRESTO.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PROJET_C#_GESTIONRESTO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1467304dd7a978c9d6fc8cc383d61c403928607")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2c7e635bf027a29e02ec4d02ba7823d05bdfe45")]
 [assembly: System.Reflection.AssemblyProductAttribute("PROJET_C#_GESTIONRESTO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PROJET_C#_GESTIONRESTO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
