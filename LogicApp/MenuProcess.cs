ca5f702ee49128f0841d1d588b8f83f5337cb21e
AMDev <miguelawoko1@gmail.com>
2024-12-18 14:15:03 +0100

    integretion of process menu in itemMenu

--- LogicApp/MenuProcess.cs
+++ LogicApp/MenuProcess.cs
@@ -1,116 +1,117 @@
-﻿//using Microsoft.Extensions.Configuration;
-//using PROJET_C__GESTIONRESTO.Models;
-//using PROJET_C__GESTIONRESTO.Orm;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Linq.Dynamic.Core;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using Microsoft.Extensions.Configuration;
+using PROJET_C__GESTIONRESTO.Models;
+using PROJET_C__GESTIONRESTO.Orm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Dynamic.Core;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace PROJET_C__GESTIONRESTO.LogicApp
-//{
-//    class MenuProcess
-//    {
-//        public readonly string? connectionString;
+namespace PROJET_C__GESTIONRESTO.LogicApp
+{
+    class MenuProcess
+    {
+        public readonly string? connectionString;
 
-//        public MenuProcess()
-//        {
-//            var configuration = ConfigurationHelper.GetConfiguration();
-//            this.connectionString = configuration.GetValue<string>("ConnectionString:MySqlConnection");
-//        }
+        public MenuProcess()
+        {
+            var configuration = ConfigurationHelper.GetConfiguration();
+            this.connectionString = configuration.GetValue<string>("ConnectionString:MySqlConnection");
+        }
 
-//        public int CreateProduct(Menu menu)
-//        {
-//            int lines = 0;
-//            using (var context = new AppDbContext(connectionString))
-//            {
-//                context.Menus.Add(menu);
-//                lines = context.SaveChanges();
-//            }
+        public int CreateProduct(Menu menu)
+        {
+            int lines = 0;
+            using (var context = new AppDbContext(connectionString))
+            {
+                context.Menus.Add(menu);
+                lines = context.SaveChanges();
+            }
 
-//            return lines;
-//        }
+            return lines;
+        }
 
-//        public int UpdateProduct(int id, Menu newMenu)
-//        {
-//            using (var context = new AppDbContext(connectionString))
-//            {
-//                int lines = 0; 
-//                var menu = context.Menus.FirstOrDefault(x => x.Id == id);
+        public int UpdateProduct(int id, Menu newMenu)
+        {
+            using (var context = new AppDbContext(connectionString))
+            {
+                int lines = 0;
+                var menu = context.Menus.FirstOrDefault(x => x.Id == id);
 
-//                if (menu != null)
-//                {
-//                    newMenu.Id = id;
-//                    menu = newMenu;
-//                    context.Menus.Update(newMenu);
-//                    lines = context.SaveChanges();
-//                }
+                if (menu != null)
+                {
+                    newMenu.Id = id;
+                    menu = newMenu;
+                    context.Menus.Update(newMenu);
+                    lines = context.SaveChanges();
+                }
 
-//                return lines;
-//            }
-//        }
+                return lines;
+            }
+        }
 
-//        public int DeleteProduct(int id)
-//        {
-//            int lines = 0;
-//            using (var context = new AppDbContext(connectionString))
-//            {
-//                var menu = context.Menus.FirstOrDefault(m => m.Id == id);
-//                if (menu != null)
-//                {
-//                    foreach (Menuitem menuitem in menu.Menuitems) { 
-//                        context.Menuitems.Remove(menuitem);
-//                    }
-//                    context.Menus.Remove(menu);
-//                    lines = context.SaveChanges();
-//                }
+        public int DeleteProduct(int id)
+        {
+            int lines = 0;
+            using (var context = new AppDbContext(connectionString))
+            {
+                var menu = context.Menus.FirstOrDefault(m => m.Id == id);
+                if (menu != null)
+                {
+                    foreach (Menuitem menuitem in menu.Menuitems)
+                    {
+                        context.Menuitems.Remove(menuitem);
+                    }
+                    context.Menus.Remove(menu);
+                    lines = context.SaveChanges();
+                }
 
-//                return lines;
-//            }
-//        }
+                return lines;
+            }
+        }
 
 
 
-//        public List<Product> GetProductItem(int menuid)
-//        {
-//            List<Product>? products = new List<Product>();
+        public List<Product> GetProductItem(int menuid)
+        {
+            List<Product>? products = new List<Product>();
 
-//            using (var context = new AppDbContext(connectionString))
-//            {
-//                var items = context.Products
-//                    .Where(p => context.Menuitems.Any(mi => mi.ProductId == p.Id && mi.MenuId == menuid))
-//                    .ToList();
+            using (var context = new AppDbContext(connectionString))
+            {
+                var items = context.Products
+                    .Where(p => context.Menuitems.Any(mi => mi.ProductId == p.Id && mi.MenuId == menuid))
+                    .ToList();
 
-//                if (items.Count > 0)
-//                {
-//                    products = items;
-//                }
-//            }
+                if (items.Count > 0)
+                {
+                    products = items;
+                }
+            }
 
-//            return products;
-//        }
+            return products;
+        }
 
-//        public List<object> FilterMenu(string searchValue)
-//        {
-//            List<object>? filteredItems = new List<object>();
+        public List<object> FilterMenu(string searchValue)
+        {
+            List<object>? filteredItems = new List<object>();
 
-//            using (var context = new AppDbContext(connectionString))
-//            {
-//                if (searchValue.GetType() == typeof(string))
-//                {
-//                    var items = context.Menus
-//                            .Where("theme.Contains(@0)", searchValue)
-//                            .ToList();
+            using (var context = new AppDbContext(connectionString))
+            {
+                if (searchValue.GetType() == typeof(string))
+                {
+                    var items = context.Menus
+                            .Where("theme.Contains(@0)", searchValue)
+                            .ToList();
 
-//                    if (items.Any())
-//                    {
-//                        filteredItems.Add(items);
-//                    }
-//                }
+                    if (items.Any())
+                    {
+                        filteredItems.Add(items);
+                    }
+                }
 
-//                return filteredItems;
-//            }
-//        }
-//    }
-//}
+                return filteredItems;
+            }
+        }
+    }
+}
--- LogicApp/ProductProcess.cs
+++ LogicApp/ProductProcess.cs
@@ -1,125 +1,128 @@
-﻿//using Microsoft.EntityFrameworkCore;
-//using Microsoft.Extensions.Configuration;
-//using PROJET_C__GESTIONRESTO.Models;
-//using PROJET_C__GESTIONRESTO.Orm;
-//using PROJET_C__GESTIONRESTO.Usefull;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Linq.Dynamic.Core;
-//using System.Reflection;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using PROJET_C__GESTIONRESTO.Models;
+using PROJET_C__GESTIONRESTO.Orm;
+using PROJET_C__GESTIONRESTO.Usefull;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Dynamic.Core;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace PROJET_C__GESTIONRESTO.LogicApp
-//{
-//    class ProductProcess
-//    {
-//        public readonly string? connectionString;
+namespace PROJET_C__GESTIONRESTO.LogicApp
+{
+    class ProductProcess
+    {
+        public readonly string? connectionString;
 
-//        public ProductProcess()
-//        {
-//            var configuration = ConfigurationHelper.GetConfiguration();
-//            this.connectionString = configuration.GetValue<string>("ConnectionString:MySqlConnection");
-//        }
+        public ProductProcess()
+        {
+            var configuration = ConfigurationHelper.GetConfiguration();
+            this.connectionString = configuration.GetValue<string>("ConnectionString:MySqlConnection");
+        }
 
-//        public int CreateProduct(Product product)
-//        {
-//            int lines = 0;
-//            using (var context = new AppDbContext(connectionString))
-//            {
-//                context.Products.Add(product);
-//                lines = context.SaveChanges();
-//            }
+        public int CreateProduct(Product product)
+        {
+            int lines = 0;
+            using (var context = new AppDbContext(connectionString))
+            {
+                context.Products.Add(product);
+                lines = context.SaveChanges();
+            }
 
-//            return lines;
-//        }
+            return lines;
+        }
 
-//        public int UpdateProduct(int id, Product newProduct)
-//        {
-//            using (var context = new AppDbContext(connectionString))
-//            {
-//                int lines = 0;
-//                var product = context.Products.FirstOrDefault(x => x.Id == id);
+        public int UpdateProduct(int id, Product newProduct)
+        {
+            using (var context = new AppDbContext(connectionString))
+            {
+                int lines = 0;
+                var product = context.Products.FirstOrDefault(x => x.Id == id);
 
-//                if (product != null) {
-//                    newProduct.Id = id;
-//                    product = newProduct;
-//                    context.Products.Update(product);
-//                    lines = context.SaveChanges();
-//                }
+                if (product != null)
+                {
+                    newProduct.Id = id;
+                    product = newProduct;
+                    context.Products.Update(product);
+                    lines = context.SaveChanges();
+                }
 
-//                return lines;
-//            }
-//        }
+                return lines;
+            }
+        }
 
-//        public int DeleteProduct(int id)
-//        {
-//            int lines = 0;
-//            using (var context = new AppDbContext(connectionString))
-//            {
-//                var product = context.Products.FirstOrDefault(p => p.Id == id);
-//                if (product != null) { 
-//                    context.Products.Remove(product);
-//                    lines = context.SaveChanges();
-//                }
+        public int DeleteProduct(int id)
+        {
+            int lines = 0;
+            using (var context = new AppDbContext(connectionString))
+            {
+                var product = context.Products.FirstOrDefault(p => p.Id == id);
+                if (product != null)
+                {
+                    context.Products.Remove(product);
+                    lines = context.SaveChanges();
+                }
 
-//                return lines;
-//            }
-//        }
+                return lines;
+            }
+        }
 
-//        public List<Product> GetDistinctProduct()
-//        {
-//            List<Product> result = null;
+        public List<Product> GetDistinctProduct()
+        {
+            List<Product> result = null;
 
-//            using (var context = new AppDbContext(connectionString))
-//            {
-//                var items = context.Products
-//                    .Where(p => context.Menuitems.Any(m => m.ProductId != p.Id))
-//                    .ToList();
+            using (var context = new AppDbContext(connectionString))
+            {
+                var items = context.Products
+                    .Where(p => context.Menuitems.Any(m => m.ProductId != p.Id))
+                    .ToList();
 
-//                if (items != null)
-//                {
-//                    result = items;
-//                }
+                if (items != null)
+                {
+                    result = items;
+                }
 
-//            }
+            }
 
-//            return result;
-//        }
+            return result;
+        }
 
-//        public List<object> FilterProduct<T>(T searchValue)
-//        {
-//            List<object>? filteredItems = new List<object>();
+        public List<object> FilterProduct<T>(T searchValue)
+        {
+            List<object>? filteredItems = new List<object>();
 
-//            using (var context = new AppDbContext(connectionString))
-//            {
-//                if (searchValue.GetType() == typeof(string))
-//                { 
-//                    var items = context.Products
-//                            .Where("designation.Contains(@0) or description.Contains(@0)", searchValue)
-//                            .ToList();
+            using (var context = new AppDbContext(connectionString))
+            {
+                if (searchValue.GetType() == typeof(string))
+                {
+                    var items = context.Products
+                            .Where("designation.Contains(@0) or description.Contains(@0)", searchValue)
+                            .ToList();
 
-//                    if(items.Any()) { 
-//                        filteredItems.Add(items);
-//                    }
-//                }
+                    if (items.Any())
+                    {
+                        filteredItems.Add(items);
+                    }
+                }
 
-//                if(searchValue.GetType() == typeof(float))
-//                {
-//                    var items = context.Products
-//                            .Where(p => p.Id.Equals(searchValue))
-//                            .ToList();
+                if (searchValue.GetType() == typeof(float))
+                {
+                    var items = context.Products
+                            .Where(p => p.Id.Equals(searchValue))
+                            .ToList();
 
-//                    if(items.Any())
-//                    {
-//                        filteredItems.Add(items);
-//                    }
-//                }  
+                    if (items.Any())
+                    {
+                        filteredItems.Add(items);
+                    }
+                }
 
-//                return filteredItems;
-//            }
+                return filteredItems;
+            }
 
-//        }
-//    }
-//}
+        }
+    }
+}
